e582cb9b0bfe8a726f9ffa64ea54221011145bf3
Peter Bartoš <xbartosp2@stuba.sk>
2025-02-25 19:49:03 +0100

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './middleware/auth/auth.guard';
-import { LoginComponent } from './components/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppWrapperComponent } from './components/app.wrapper';
 
 // Define routes
 export const routeConfig: Routes = [
-  { path: 'auth', component: LoginComponent },
   {
     path: '',
-    component: DashboardComponent,
-    canActivate: [authGuard]
+    component: AppWrapperComponent,
   },
-  { path: '**', redirectTo: 'auth'}  // Catch-all redirect to /auth
+  { path: '**', redirectTo: ''}  // Catch-all redirects
 ];
---
+++ src/app/components/app.wrapper.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Auth, onAuthStateChanged, User } from '@angular/fire/auth';
+
+@Component({
+  selector: 'app-wrapper',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<ng-container *ngComponentOutlet="component"></ng-container>`,
+})
+
+export class AppWrapperComponent {
+  private auth = inject(Auth);
+  component: any = LoginComponent;
+
+  constructor() {
+    onAuthStateChanged(this.auth, (user: User | null) => {
+      this.component = user ? DashboardComponent : LoginComponent;
+    });
+  }
+}
